c26173b08426bc564a475ad123660138b9764585
CJ Avilla <cjavilla@gmail.com>
2016-06-12 20:02:02 -0700

    unhide those

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login!, only: [:welcome]
+  skip_before_action :require_login!, except: [:root]
   def root
   end
 
